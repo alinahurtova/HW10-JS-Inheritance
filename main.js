7f40f4053ef9dad2701ea84a4ce5eea1779f1295
alinahurtova <alina.hurtova@gmail.com>
2017-09-01 19:22:13 +0300

    correction

--- main.js
+++ main.js
@@ -1,33 +1,30 @@
 var Gladiator = function (name) {
     this.name = name;
-    this.attack = getAttack();
-        function getAttack () {
-            return Math.floor(Math.random() * (10 - 1)) + 1;
-        }
-    this.hitpoints = getPoints();
-        function getPoints () {
-            return Math.floor(Math.random() * (101 - 80)) + 80;
-        }
+
+    this.attack = function () {
+        return (Math.floor(Math.random() * (10 - 1)) + 1);
+    };
+    this.hitpoints = function () {
+        return (Math.floor(Math.random() * (101 - 80)) + 80);
+    };
     this.setAttack = function () {
-        console.log(this.name + ' аттакует с силой ' + this.attack);
-    }
+        console.log(this.name + ' аттакует с силой ' + this.attack());
+    };
     this.setHitpoints = function () {
-        console.log('У ' + this.name + ' ' + this.hitpoints + ' очков');
-    }
+        console.log(this.name + ' - ' + this.hitpoints() + ' очков');
+    };
     this.fight = function (enemy) {
-        this.setAttack();
-        enemy.setHitpoints();
-        return enemy.hitpoints - this.attack;
-    }
-}
+        console.log(enemy.hitpoints() - this.attack());
+    };
+};
 
 var Champion = function (name) {
     Gladiator.apply(this, arguments);
-}
+};
 
 var Monster = function (name) {
     Gladiator.apply(this, arguments);
-}
+};
 
 Champion.prototype = Gladiator();
 Monster.prototype = Gladiator();
@@ -36,3 +33,4 @@ var heracles = new Champion('Геракл');
 var boar = new Monster('BOAR');
 
 console.log( boar.fight(heracles) );
+
